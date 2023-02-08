0671511a98e28f684b9abe8ecd662683ba2119bc
shilivvi <vanya.ivanov161@gmail.com>
2023-02-08 22:20:31 +0300

    Added db settings and require db's class

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DBHost', '127.0.0.1');
+define('DBPort', 3306);
+define('DBName', 'slmax');
+define('DBUser', 'root');
+define('DBPassword', '');
+
+require_once(__DIR__ . "/src/PDO.class.php");
